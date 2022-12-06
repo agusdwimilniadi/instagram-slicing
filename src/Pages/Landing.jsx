0eb07afd0cb541160a623c753287017cdaa97810
agusdwimilniadi <agusdwimill@gmail.com>
2022-12-06 22:41:51 +0700

    slicing instagram done

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import photoProfile from "./profile.jpeg";
+import CardPost from "../Components/CardPost";
+import axios from "axios";
 
 const Landing = () => {
+  const [data, setData] = useState({
+    comments: "",
+  });
+  const [dataPostingan, setDataPostingan] = useState([]);
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const fetchDataPostingan = async () => {
+    const dataApi = await axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .catch((err) => {
+        console.log(err);
+      });
+    setDataPostingan(dataApi.data);
+    // console.log(dataApi.data);
+  };
+  useEffect(() => {
+    fetchDataPostingan();
+  }, []);
+
   return (
     <>
       <section id="story">
@@ -9,77 +35,117 @@ const Landing = () => {
           <div className="row">
             <div className="col-6 offset-3 ">
               <div className="row bg-white">
-                <div
-                  className="col-md-12 mt-3 rounded border-story"
-                  style={{ overflow: "scroll", width: "100vh" }}
-                >
-                  <div>
-                    <span>
+                <div className="col-md-12 mt-3 rounded border-story">
+                  <div style={{ overflow: "auto", whiteSpace: "nowrap" }}>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                    <span>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                    <span>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                    <span>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                    <span>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                    <span>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
                       <img
                         src={photoProfile}
                         alt="profile"
-                        className="img-fluid rounded-circle story-profile m-3"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
                       />
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="row bg-white my-3 ">
-                <div className="col-md-12 border-story rounded">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="row d-flex align-items-center">
-                        <div className="col-1">
-                          <img
-                            src={photoProfile}
-                            alt="image"
-                            className="img-fluid rounded-circle image-feed my-3"
-                          />
-                        </div>
-                        <div className="col-9">
-                          <p className="fw-bold">agusdwimilniadi</p>
-                        </div>
-                      </div>
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
+                      <img
+                        src={photoProfile}
+                        alt="profile"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
+                      />
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
+                    </div>
+                    <div
+                      className="text-center"
+                      style={{ display: "inline-block" }}
+                    >
+                      <img
+                        src={photoProfile}
+                        alt="profile"
+                        className="img-fluid rounded-circle story-profile mx-2 my-1  border-gradient"
+                      />
+                      <br />
+                      <span className="fw-bold limit fs-6">agusdm</span>
                     </div>
                   </div>
                 </div>
               </div>
+              {dataPostingan.slice(0, 100).map((item) => {
+                return (
+                  <CardPost
+                    image={item.thumbnailUrl}
+                    dataProps={data}
+                    caption={item.title}
+                    handleChangeProps={handleChange}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
